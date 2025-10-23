c8f6307de4fae16a2fd5c38e5e660d72bef41178
Shuja-2406 <240541608@firat.edu.tr>
2025-10-23 12:40:13 +0300

    Add files via upload

--- Task-3-GeometrikHesaplamalar/App.java
+++ Task-3-GeometrikHesaplamalar/App.java
@@ -1,10 +1,12 @@
 public class App {
-    public static void main(String[] args) {
-        double yaricap = 5.5;
-        double alan = Math.PI * yaricap * yaricap;
-        double cevre = 2 * Math.PI * yaricap;
+    public static void main(String[] args) throws Exception {
+        double en = 4.5;
+        double boy = 7.9;
 
-        System.out.printf("Yarıçap: %.2f%n", yaricap);
+        double alan = en * boy;
+        double cevre = 2 * (en + boy);
+
+        System.out.printf("En: %.2f, Boy: %.2f%n", en, boy);
         System.out.printf("Alan: %.4f%n", alan);
         System.out.printf("Çevre: %.4f%n", cevre);
     }
