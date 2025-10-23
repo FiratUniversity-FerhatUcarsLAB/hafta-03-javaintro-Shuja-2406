29c88458c21a2d603b5b8649f29322b9e9888545
Shuja-2406 <240541608@firat.edu.tr>
2025-10-23 12:13:55 +0300

    Add files via upload

---
+++ Task-1-HosgeldinMesajlari/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("  JJJJJ    AAAAA    V     V    AAAAA");
+        System.out.println("    J      A   A     V   V     A   A");
+        System.out.println("    J      A   A      V V      A   A");
+        System.out.println("    J      AAAAA       V       AAAAA");
+        System.out.println("J   J      A   A       V       A   A");
+        System.out.println(" JJJ       A   A       V       A   A");
+    }
+}
