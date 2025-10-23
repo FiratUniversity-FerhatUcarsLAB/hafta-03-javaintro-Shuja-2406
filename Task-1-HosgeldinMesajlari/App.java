7959216fed533bc5b1220dd1f288d8c1408406fb
Shuja-2406 <240541608@firat.edu.tr>
2025-10-23 11:45:32 +0300

    Add files via upload

---
+++ Task-1-HosgeldinMesajlari/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Java'ya Hos Geldin !!");
+        System.out.println("Java'ya Hos Geldin !!");
+        System.out.println("Java'ya Hos Geldin !!");
+    }
+}
