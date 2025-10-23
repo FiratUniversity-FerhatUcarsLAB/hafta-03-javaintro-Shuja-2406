1b22d4f6ca03c1fcf9414d44e083d53f5b2e45f3
Shuja-2406 <240541608@firat.edu.tr>
2025-10-23 12:55:47 +0300

    Add files via upload

--- Task-4-StringDesenler/App.java
+++ Task-4-StringDesenler/App.java
@@ -1,17 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        String ad = "Shuja";
-        String soyad = "Tariq";
-        String bolum = "Software Engineering";
-        String ogrenciNo = "240541608";
-
-        System.out.println("=====================================");
-        System.out.println("           ÖĞRENCİ KARTI             ");
-        System.out.println("=====================================");
-        System.out.println("Ad           : " + ad);
-        System.out.println("Soyad        : " + soyad);
-        System.out.println("Öğrenci No   : " + ogrenciNo);
-        System.out.println("Bölüm        : " + bolum);
-        System.out.println("=====================================");
+        System.out.println("  SSSSS  ");
+        System.out.println(" S     S ");
+        System.out.println(" S       ");
+        System.out.println("  SSSSS  ");
+        System.out.println("       S ");
+        System.out.println(" S     S ");
+        System.out.println("  SSSSS  ");
     }
 }
