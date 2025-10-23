dc4f7c0146d487310ebcfd162ce0f563df95af4f
Shuja-2406 <240541608@firat.edu.tr>
2025-10-23 12:51:33 +0300

    Add files via upload

---
+++ Task-4-StringDesenler/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String ad = "Shuja";
+        String soyad = "Tariq";
+        String bolum = "Software Engineering";
+        String ogrenciNo = "240541608";
+
+        System.out.println("=====================================");
+        System.out.println("           ÖĞRENCİ KARTI             ");
+        System.out.println("=====================================");
+        System.out.println("Ad           : " + ad);
+        System.out.println("Soyad        : " + soyad);
+        System.out.println("Öğrenci No   : " + ogrenciNo);
+        System.out.println("Bölüm        : " + bolum);
+        System.out.println("=====================================");
+    }
+}
