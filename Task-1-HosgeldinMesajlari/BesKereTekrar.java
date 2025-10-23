acea945b50ae1d3f7264448ebc73167a49a1ac10
Shuja-2406 <240541608@firat.edu.tr>
2025-10-23 12:07:50 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        
+    }
+}
