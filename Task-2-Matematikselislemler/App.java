bd2df7663b3f56dd17eb13db2d6411d056897e7c
Shuja-2406 <240541608@firat.edu.tr>
2025-10-23 12:26:45 +0300

    Add files via upload

--- Task-2-Matematikselislemler/App.java
+++ Task-2-Matematikselislemler/App.java
@@ -1,6 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        int toplam = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
-        System.out.println("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 = " + toplam);
+         double pay = (9.5 * 4.5) - (2.5 * 3);   // 42.75 - 7.5 = 35.25
+        double payda = 45.5 - 3.5;             // 42.0
+        double sonuc = pay / payda;            // ~0.839285714...
+
+        System.out.printf("Pay: %.4f%n", pay);
+        System.out.printf("Payda: %.4f%n", payda);
+        System.out.printf("Sonuç (4 ondalık basamak): %.4f%n", sonuc);
     }
 }
