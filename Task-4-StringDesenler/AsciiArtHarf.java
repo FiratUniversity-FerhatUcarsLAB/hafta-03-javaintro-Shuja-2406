9f3bed807d03ce7e79a2fb896f3476ef801e213e
Shuja-2406 <240541608@firat.edu.tr>
2025-10-23 12:55:13 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("  SSSSS  ");
+        System.out.println(" S     S ");
+        System.out.println(" S       ");
+        System.out.println("  SSSSS  ");
+        System.out.println("       S ");
+        System.out.println(" S     S ");
+        System.out.println("  SSSSS  ");
+    }
+}
